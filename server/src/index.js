4ac8a947905ecb924ff9f2e9770461adb15be3a3
kush <kushal27001@gmail.com>
2025-06-20 21:25:23 +0530

    Deployment error fixed

--- server/src/index.js
+++ server/src/index.js
@@ -35,7 +35,7 @@ const PORT = process.env.PORT || 5000;
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../../client/dist')));
     
-app.get('*', (req, res) => {
+app.get(/^\/(?!api).*/, (req, res) => {
         res.sendFile(path.join(__dirname, '../../client','dist','index.html'));
     });
 }
